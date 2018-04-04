9d478c32647f3e9329b9bb8ba6e51e350520923a
Senk <paweldawidziak@gmail.com>
2018-04-04 11:18:43 +0200

    41 Done
    I made a prefab from my Rocket object
    I did first steps to move the rocket by clicking W, A, D keys using a new script.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ProcessInput();
+		
+	}
+
+    private void ProcessInput()
+    {
+        //DO PRZODU
+        if(Input.GetKey(KeyCode.W))
+        {
+            print("^");
+        }
+        //BOKI 
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("<");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print(">");
+        }
+    }
+}
