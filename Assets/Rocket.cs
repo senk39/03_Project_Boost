74ac8804feb1c039fff4b9efada1dd57ca7cc895
Senk <paweldawidziak@gmail.com>
2018-04-16 19:16:15 +0200

    Revert "Lesson 57 - My bad code"
    - commited because of conflict in the rocket.cs
    This reverts commit 8c1fc9a32f62f2419165b1cf3839d9c42eb91dfd.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class Rocket : MonoBehaviour
 {
 
+<<<<<<< HEAD
     // todo fix lighting bug
     [SerializeField]
     float rcsThrust = 100f;
@@ -18,6 +19,20 @@ public class Rocket : MonoBehaviour
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+=======
+    int currentScene = 0;
+    bool isSteeringFreezed = false;
+
+    enum CurrentStatus { Alive, Dying, Transcending};
+    CurrentStatus currentStatus = CurrentStatus.Alive;
+
+    [SerializeField]
+    float rcsThrust = 250f;
+    [SerializeField]
+    float upThrust = 550f;
+
+    Rigidbody rigidbody;
+>>>>>>> parent of 8c1fc9a... Lesson 57 - My bad code
 
     enum State { Alive, Dying, Transcending }
     State state = State.Alive;
@@ -46,6 +61,7 @@ public class Rocket : MonoBehaviour
 
         switch (collision.gameObject.tag)
         {
+<<<<<<< HEAD
             case "Friendly":
                 // do nothing
                 break;
@@ -64,6 +80,10 @@ public class Rocket : MonoBehaviour
         audioSource.Stop();
         audioSource.PlayOneShot(success);
         Invoke("LoadNextLevel", 1f); // parameterise time
+=======
+            rigidbody.AddRelativeForce(Vector3.up * upThrust);
+        }
+>>>>>>> parent of 8c1fc9a... Lesson 57 - My bad code
     }
 
     private void StartDeathSequence()
