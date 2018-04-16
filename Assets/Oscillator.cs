b5d92c47d7cf753c5295faa58a295a3980bea789
Senk <paweldawidziak@gmail.com>
2018-04-16 21:39:21 +0200

    Lesson 59 WIP
    To do movement of obstacle

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField]
+    Vector3 movementVector;
+    [Range(0,1)]
+    [SerializeField]
+    float movementFactor;
+
+    //Vector3 startingPos = (0.0f, 0.0f, 0.0f); TODO
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
