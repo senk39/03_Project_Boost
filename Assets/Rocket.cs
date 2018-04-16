4585d560d7d1e736093d85828d0e49ad31a3a4c1
Senk <paweldawidziak@gmail.com>
2018-04-16 16:58:16 +0200

    Lesson 57 - Correct code from teacher

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,150 +1,124 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
 
-    int currentScene = 0;
-    bool isSteeringFreezed = false;
+    // todo fix lighting bug
+    [SerializeField]
+    float rcsThrust = 100f;
+    [SerializeField]
+    float mainThrust = 100f;
+    [SerializeField]
+    AudioClip mainEngine;
+    [SerializeField]
+    AudioClip success;
+    [SerializeField]
+    AudioClip death;
 
-    enum CurrentStatus { Alive, Dying, Transcending};
-    CurrentStatus currentStatus = CurrentStatus.Alive;
-
-    [SerializeField] float rcsThrust = 250f;
-    [SerializeField] float upThrust = 550f;
-    [SerializeField] AudioClip mainEngine;
-
-
-    Rigidbody rigidbody;
+    Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
 
     // Use this for initialization
     void Start()
     {
-
-        rigidbody = GetComponent<Rigidbody>();
-
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
-    }
-
-    private void ProcessInput()
-    {
-        AudioSource audio = GetComponent<AudioSource>();
-
-        //Controlling the rocket
-        Thrust();
-        Steering();
-
-        //Audio
-        PlayRocketSound(audio);
-        StopRocketSound(audio);
-    }
-
-    //Controlling the rocket
-    private void Thrust()
-    {
-        if (Input.GetKey(KeyCode.W) && (isSteeringFreezed == false))
+        // todo somewhere stop sound on death
+        if (state == State.Alive)
         {
-            rigidbody.AddRelativeForce(Vector3.up * upThrust);
-            
-            if (!audioSource.isPlaying)
-            {
-                audioSource.PlayOneShot(mainEngine);
-            }
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive) { return; } // ignore collisions when dead
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // do nothing
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        Invoke("LoadNextLevel", 1f); // parameterise time
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstLevel", 1f); // parameterise time
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1); // todo allow for more than 2 levels
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+            ApplyThrust();
         }
         else
         {
             audioSource.Stop();
-        
         }
-
     }
-    private void Steering()
+
+    private void ApplyThrust()
     {
-        rigidbody.freezeRotation = true; // take manual control of rotation
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying) // so it doesn't layer
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    private void RespondToRotateInput()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
 
         float rotationThisFrame = rcsThrust * Time.deltaTime;
-
-
-        if (Input.GetKey(KeyCode.A) && (isSteeringFreezed == false))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward * rotationThisFrame);
         }
-        else if (Input.GetKey(KeyCode.D) && (isSteeringFreezed == false))
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
-        rigidbody.freezeRotation = false; // take manual control of rotation
-    }
-    
-    //Audio
-    private static void PlayRocketSound(AudioSource audio)
-    {
-        if (!audio.isPlaying)
-        {
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                audio.Play();
-            }
-        }
-    }
-    private static void StopRocketSound(AudioSource audio)
-    {
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            audio.Stop();
-        }
-    }
-
-    //Collision
-    void OnCollisionEnter(Collision collision)
-    {
-        if(currentStatus != CurrentStatus.Alive)
-        {
-            return;
-        }
-        else if(collision.gameObject.tag == "Friendly")
-        {
-            return;
-        }
-
-        else if (collision.gameObject.tag == "Finish")
-        {
-            isSteeringFreezed = true;
-            currentStatus = CurrentStatus.Transcending;
-            print("CONGRATS!");
-            Invoke("LoadNextLevel", 1f);
-        }
-        else // literally Sayori
-        {
-            isSteeringFreezed = true;
-            currentStatus = CurrentStatus.Dying;
-            print("aua :(");
-            Invoke("RepeatLevel", 1f);
-        }
-    }
-
-    //Load Level
-    void LoadNextLevel()
-    {
-        isSteeringFreezed = false;
-        currentScene++;
-        SceneManager.LoadScene(currentScene);
-    }
-
-    void RepeatLevel()
-    {
-        isSteeringFreezed = false;
-
-        SceneManager.LoadScene(currentScene);
+        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
 }
